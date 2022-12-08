b10e6c73e940f4a38c13834a0682a385a88fcc8b
jikkenjo <oseki.kenichi@takayoshi.co.jp>
2022-12-08 21:10:15 +0900

    pc版の動き完成したよ！

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
-var carousel = $(".carousel"),
-    currdeg  = 0;
+let carousel = $(".carousel"),
+      currdeg  = 0;
 
 $(".next").on("click", { d: "n" }, rotate);
 $(".prev").on("click", { d: "p" }, rotate);
@@ -18,3 +18,31 @@ function rotate(e){
     "transform": "rotateY("+currdeg+"deg)"
   });
 }
+
+// 4秒ごとに「next」のボタンを押す
+autoFlipster = function(){
+  $('.next').click();
+};
+const timerID = setInterval(autoFlipster, 4000);
+// 4秒ごとに「next」のボタンを押す END
+
+
+// スライドの全体数を取得
+// let slide_count_all = element.childElementCount;
+let slide_count_all = document.querySelector('#carousel').childElementCount;
+document.getElementById('count_num_all').innerText = "0" + slide_count_all;
+// スライドの全体数を取得 END
+
+
+// 数字のカウント
+let i = 1;
+setInterval(countup, 4000);
+
+function countup() {
+i++;
+  if (i > 3) { i = 1; }
+  let num_text = "0" + i;
+  document.getElementById('count_num_now').innerText = num_text;
+}
+// 数字のカウント END
+
